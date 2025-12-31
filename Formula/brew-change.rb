bad0621000e96c40843e3def925294650f78b4b1
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2025-12-31 18:20:14 +0800

    chore(brew-change): bump to version 1.5.3
    Co-Authored-By: GLM <zai-org@users.noreply.github.com>

--- Formula/brew-change.rb
+++ Formula/brew-change.rb
@@ -1,8 +1,8 @@
 class BrewChange < Formula
   desc "Make informed updates - see what changed in your Homebrew packages"
   homepage "https://github.com/shrwnsan/brew-change"
-  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.5.1.tar.gz"
-  sha256 "d054c6bde29f538e121bf9f497cad16161fd56cd59ccc75e642d6f4f4fa7d2a2"
+  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.5.3.tar.gz"
+  sha256 "5ac6498438e565e4270453c7437e09a7e6645cf57bf9ff1484f45e3978f58733"
   license "Apache-2.0"
   head "https://github.com/shrwnsan/brew-change.git", branch: "main"
 
