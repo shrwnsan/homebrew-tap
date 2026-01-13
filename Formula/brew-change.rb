cc610ef648a970474a43f7b032a959c33f270aa4
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2026-01-13 10:41:58 +0800

    chore(brew-change): bump to version 1.5.10
    Improves UX when gh CLI is unauthenticated
    
    Co-Authored-By: GLM <zai-org@users.noreply.github.com>

--- Formula/brew-change.rb
+++ Formula/brew-change.rb
@@ -1,8 +1,8 @@
 class BrewChange < Formula
   desc "Make informed updates - see what changed in your Homebrew packages"
   homepage "https://github.com/shrwnsan/brew-change"
-  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.5.9.tar.gz"
-  sha256 "14087e7ee98a4680f53548b573b31ce380748059adffbd56e523732cb4486c7c"
+  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.5.10.tar.gz"
+  sha256 "7f4258eb1f76e44e9cdb486a948336d107b897c2e831d8f34d8ba20f6649c05b"
   license "Apache-2.0"
   head "https://github.com/shrwnsan/brew-change.git", branch: "main"
 
