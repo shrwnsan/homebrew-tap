9c412f6a9e903d42a0cc2e14c1d19bb315f1783d
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2026-02-12 17:12:44 +0800

    fix(glow): remove bottle block to build from source
    The referenced bottle files don't exist in the release. Remove the bottle
    block so Homebrew builds from source instead.
    
    Co-Authored-By: GLM <zai-org@users.noreply.github.com>

--- Formula/glow.rb
+++ Formula/glow.rb
@@ -7,16 +7,6 @@ class Glow < Formula
 
   depends_on "go" => :build
 
-  bottle do
-    root_url "https://github.com/shrwnsan/glow/releases/download/v2.1.2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "75a8935ff7e94ac46a7b3d389022298b893adc617e889828f29f7859e48cb5fe"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "75a8935ff7e94ac46a7b3d389022298b893adc617e889828f29f7859e48cb5fe"
-    sha256 cellar: :any_skip_relocation, arm64_monterey:  "75a8935ff7e94ac46a7b3d389022298b893adc617e889828f29f7859e48cb5fe"
-    sha256 cellar: :any_skip_relocation, sonoma: "c9ce8439c58e5cd987a82d8f7192a5fda05052467cd6f40c89255b8f4e18bebd"
-    sha256 cellar: :any_skip_relocation, ventura:    "c9ce8439c58e5cd987a82d8f7192a5fda05052467cd6f40c89255b8f4e18bebd"
-    sha256 cellar: :any_skip_relocation, monterey:   "c9ce8439c58e5cd987a82d8f7192a5fda05052467cd6f40c89255b8f4e18bebd"
-  end
-
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.Version=#{version}")
 
