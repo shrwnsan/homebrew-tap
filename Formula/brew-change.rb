a93c961d58f9d02e12e89e86bafb34300b527831
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2026-07-01 15:00:45 +0800

    chore(brew-change): bump to version 1.8.1 (#5)
    Skip changelog confirmation prompt in upgrade mode
    
    Co-authored-by: GLM <zai-org@users.noreply.github.com>

--- Formula/brew-change.rb
+++ Formula/brew-change.rb
@@ -1,8 +1,8 @@
 class BrewChange < Formula
   desc "Make informed updates - see what changed in your Homebrew packages"
   homepage "https://github.com/shrwnsan/brew-change"
-  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.8.0.tar.gz"
-  sha256 "1afb539990317d5d9a578cb62016d27d343751118f88101cfe9264c27f457202"
+  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.8.1.tar.gz"
+  sha256 "c47c24cfabd6fcaa83547d4e4fb3e4ac5d046ab537c34fee1a7bc6f2d434e946"
   license "Apache-2.0"
   head "https://github.com/shrwnsan/brew-change.git", branch: "main"
 
