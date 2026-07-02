e4cb417e8494d0566e708b33b59e8f9b195c6198
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2026-07-02 22:34:36 +0800

    chore(brew-change): bump to version 1.8.3
    Fix interactive prompts writing to /dev/tty
    
    Co-Authored-By: GLM <zai-org@users.noreply.github.com>

--- Formula/brew-change.rb
+++ Formula/brew-change.rb
@@ -1,8 +1,8 @@
 class BrewChange < Formula
   desc "Make informed updates - see what changed in your Homebrew packages"
   homepage "https://github.com/shrwnsan/brew-change"
-  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.8.2.tar.gz"
-  sha256 "4fe979125e7923df0f2085c1fc61d057787b21ab1cfacde28d2288de5ad52c18"
+  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.8.3.tar.gz"
+  sha256 "87eaeed94647633478420b56e4d67ff56cf051f63d69870376f2b9d6fc3b0f59"
   license "Apache-2.0"
   head "https://github.com/shrwnsan/brew-change.git", branch: "main"
 
