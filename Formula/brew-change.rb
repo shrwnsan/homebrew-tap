edf22835d41cf0a7c9afd0a437024dfe1cdd10ac
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2026-07-03 00:17:22 +0800

    chore(brew-change): bump to version 1.8.4
    Add spinner + timer animation to upgrade action prompt
    
    Co-Authored-By: GLM <zai-org@users.noreply.github.com>

--- Formula/brew-change.rb
+++ Formula/brew-change.rb
@@ -1,8 +1,8 @@
 class BrewChange < Formula
   desc "Make informed updates - see what changed in your Homebrew packages"
   homepage "https://github.com/shrwnsan/brew-change"
-  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.8.3.tar.gz"
-  sha256 "87eaeed94647633478420b56e4d67ff56cf051f63d69870376f2b9d6fc3b0f59"
+  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.8.4.tar.gz"
+  sha256 "cc031c7b05a1f7aa3cdbc0e5168da55cebe0951ad4dc958787636c3a801514e1"
   license "Apache-2.0"
   head "https://github.com/shrwnsan/brew-change.git", branch: "main"
 
