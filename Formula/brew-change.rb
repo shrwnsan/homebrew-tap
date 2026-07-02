08e5a740ffe4d135f549384b45b9bc7e7d84a2bf
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2026-07-02 17:04:55 +0800

    chore(brew-change): bump to version 1.8.2
    Improve interactive prompt timeout and visibility
    
    Co-Authored-By: GLM <zai-org@users.noreply.github.com>

--- Formula/brew-change.rb
+++ Formula/brew-change.rb
@@ -1,8 +1,8 @@
 class BrewChange < Formula
   desc "Make informed updates - see what changed in your Homebrew packages"
   homepage "https://github.com/shrwnsan/brew-change"
-  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.8.1.tar.gz"
-  sha256 "c47c24cfabd6fcaa83547d4e4fb3e4ac5d046ab537c34fee1a7bc6f2d434e946"
+  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.8.2.tar.gz"
+  sha256 "4fe979125e7923df0f2085c1fc61d057787b21ab1cfacde28d2288de5ad52c18"
   license "Apache-2.0"
   head "https://github.com/shrwnsan/brew-change.git", branch: "main"
 
