16405a9ae1170cd18a14b25cb7b69fb789d46771
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2026-07-03 00:40:50 +0800

    chore(brew-change): bump to version 1.8.5 (#9)
    Fix single-threaded spinner for upgrade action prompt
    
    Co-authored-by: GLM <zai-org@users.noreply.github.com>

--- Formula/brew-change.rb
+++ Formula/brew-change.rb
@@ -1,8 +1,8 @@
 class BrewChange < Formula
   desc "Make informed updates - see what changed in your Homebrew packages"
   homepage "https://github.com/shrwnsan/brew-change"
-  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.8.4.tar.gz"
-  sha256 "cc031c7b05a1f7aa3cdbc0e5168da55cebe0951ad4dc958787636c3a801514e1"
+  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.8.5.tar.gz"
+  sha256 "3a3974f9d2013bedcffabb149fbab4659a36e14df37fd1698c4aa23dbee53335"
   license "Apache-2.0"
   head "https://github.com/shrwnsan/brew-change.git", branch: "main"
 
