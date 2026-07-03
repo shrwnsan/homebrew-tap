9593af6f8cc7eea1f3b7c3fec61cefbb997c1917
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2026-07-03 22:54:33 +0800

    chore(brew-change): bump to version 1.9.1
    Remove redundant confirmation prompt
    
    Co-Authored-By: GLM <zai-org@users.noreply.github.com>

--- Formula/brew-change.rb
+++ Formula/brew-change.rb
@@ -1,8 +1,8 @@
 class BrewChange < Formula
   desc "Make informed updates - see what changed in your Homebrew packages"
   homepage "https://github.com/shrwnsan/brew-change"
-  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.9.0.tar.gz"
-  sha256 "7f91cef31b1904004cbf8ec778414c08a8768c700faee9cb653924fb9b18e82c"
+  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.9.1.tar.gz"
+  sha256 "2ea6dc9775a0dc4264e2f1c3514f403ce1216ad4a17e3b401fc2fa45388ac55f"
   license "Apache-2.0"
   head "https://github.com/shrwnsan/brew-change.git", branch: "main"
 
