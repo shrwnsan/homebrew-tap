ae33ad0aa06fb62338624fd38cd52a67ddab5bd3
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2026-07-03 17:11:10 +0800

    chore(brew-change): bump to version 1.8.7
    Change spinner timer to countdown
    
    Co-Authored-By: GLM <zai-org@users.noreply.github.com>

--- Formula/brew-change.rb
+++ Formula/brew-change.rb
@@ -1,8 +1,8 @@
 class BrewChange < Formula
   desc "Make informed updates - see what changed in your Homebrew packages"
   homepage "https://github.com/shrwnsan/brew-change"
-  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.8.6.tar.gz"
-  sha256 "92781645cf77399665cb85f868528227ca03ec6fbf0909e4f983ae97003e113b"
+  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.8.7.tar.gz"
+  sha256 "ceb9db34ec4c8871845be8401fd43db4d8af8954ff2986218e8aafa0a7823aa7"
   license "Apache-2.0"
   head "https://github.com/shrwnsan/brew-change.git", branch: "main"
 
