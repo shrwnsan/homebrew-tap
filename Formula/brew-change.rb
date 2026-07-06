468e6c7e8eff79efeb4432f355fd5e7a6bdc1057
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2026-07-07 02:31:32 +0800

    chore(brew-change): bump to version 1.10.0
    Improve interactive prompt UX
    
    Co-Authored-By: GLM <zai-org@users.noreply.github.com>

--- Formula/brew-change.rb
+++ Formula/brew-change.rb
@@ -1,8 +1,8 @@
 class BrewChange < Formula
   desc "Make informed updates - see what changed in your Homebrew packages"
   homepage "https://github.com/shrwnsan/brew-change"
-  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.9.2.tar.gz"
-  sha256 "90efa4f996c371069de29be14e9d1456b692c46159479b740b400e16bece6fbf"
+  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.10.0.tar.gz"
+  sha256 "65706344559ea6b439f7a116270f44657ca5a156cd933a3be25aff3eace01742"
   license "Apache-2.0"
   head "https://github.com/shrwnsan/brew-change.git", branch: "main"
 
