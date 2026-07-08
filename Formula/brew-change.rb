8af163625df877f96b9aa6d1af3399176fdd2f3a
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2026-07-08 23:06:46 +0800

    brew-change: bump to 1.11.1
    Co-Authored-By: GLM <zai-org@users.noreply.github.com>

--- Formula/brew-change.rb
+++ Formula/brew-change.rb
@@ -1,8 +1,8 @@
 class BrewChange < Formula
   desc "Make informed updates - see what changed in your Homebrew packages"
   homepage "https://github.com/shrwnsan/brew-change"
-  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.11.0.tar.gz"
-  sha256 "4dae317554061c5f88299defd78389ac2e7732197006b4864db192227757763d"
+  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.11.1.tar.gz"
+  sha256 "d8b72f0ec4d2a62f37d52d86a2ffc6f8c0ce10e36372c6bfb31d948c0655085f"
   license "Apache-2.0"
   head "https://github.com/shrwnsan/brew-change.git", branch: "main"
 
