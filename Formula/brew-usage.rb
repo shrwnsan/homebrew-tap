9e16b641caefa63f30de5214c1bad11c92a8d92b
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2026-08-18 02:42:41 +0800

    bump(brew-usage): 0.3.0 (#20)
    Co-authored-by: GLM <zai-org@users.noreply.github.com>

--- Formula/brew-usage.rb
+++ Formula/brew-usage.rb
@@ -1,8 +1,8 @@
 class BrewUsage < Formula
   desc "Homebrew Disk Usage Analyzer - Shows disk usage information for installed Homebrew packages"
   homepage "https://github.com/shrwnsan/brew-usage#readme"
-  url "https://github.com/shrwnsan/brew-usage/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "760db31315adaecaee6e21597585e47ef12566b908a9478bab5ceb42acd0b91a"
+  url "https://github.com/shrwnsan/brew-usage/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "3e0f698073538f6f8855aa4f5ea20a757ce0a6957213a9a180e71c063f7076a1"
   license "Apache-2.0"
   head "https://github.com/shrwnsan/brew-usage.git", branch: "main"
 
