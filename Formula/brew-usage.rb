2b305a843f7d06820c19deacb0fbd245e278d7c2
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2026-08-21 02:16:37 +0800

    brew-usage 0.13.0, brew-change 1.17.0

--- Formula/brew-usage.rb
+++ Formula/brew-usage.rb
@@ -1,8 +1,8 @@
 class BrewUsage < Formula
   desc "Homebrew Disk Usage Analyzer - Shows disk usage information for installed Homebrew packages"
   homepage "https://github.com/shrwnsan/brew-usage#readme"
-  url "https://github.com/shrwnsan/brew-usage/archive/refs/tags/v0.11.0.tar.gz"
-  sha256 "dc59299f7b9e77a4026618ac78785b485b229d05e51716419343d3712031e2f4"
+  url "https://github.com/shrwnsan/brew-usage/archive/refs/tags/v0.13.0.tar.gz"
+  sha256 "c46922f93fa50fdad6159f8bb804b02f2e0af8bffb865f0a642da5772f759811"
   license "Apache-2.0"
   head "https://github.com/shrwnsan/brew-usage.git", branch: "main"
 
