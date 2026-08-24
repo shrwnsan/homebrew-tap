bec8b19d441daba205511a97efd87bac610352de
shrwnsan <38465+shrwnsan@users.noreply.github.com>
2026-08-25 00:47:18 +0800

    fix(brew-change): bump to version 1.18.1

--- Formula/brew-change.rb
+++ Formula/brew-change.rb
@@ -1,8 +1,8 @@
 class BrewChange < Formula
   desc "Make informed updates - see what changed in your Homebrew packages"
   homepage "https://github.com/shrwnsan/brew-change"
-  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.18.0.tar.gz"
-  sha256 "1ad378ab9c44fe754a9e938f5ac05331c8064595ae2666b860d9d7cd42b89276"
+  url "https://github.com/shrwnsan/brew-change/archive/refs/tags/v1.18.1.tar.gz"
+  sha256 "9588e0cfd9440bffba833d511c4e8230af6295842ffeb1d2d38dd0d6f8befe08"
   license "Apache-2.0"
   head "https://github.com/shrwnsan/brew-change.git", branch: "main"
 
